9c07f7b996026ce1dbc6e2a3cddd186710a7297c
IrvingPaim <irvingpaim@DESKTOP-UJMKT69>
2023-10-22 20:35:15 -0300

    passos

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import '../css/date-counter.css'
-import { AppCounter } from './components/AppCounter'
+import '../css/steps.css'
+import { AppPassos } from './components/AppPassos'
 
 function App() {
 	return (
 		<>
-        <AppCounter />
+        <AppPassos />
 		</>
 	)
 }
